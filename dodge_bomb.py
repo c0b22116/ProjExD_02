4edb70b8865133ecae220c2622b6af28fc835443
Nishimura Takuma <c0b2211617@edu.teu.ac.jp>
2023-04-25 14:53:57 +0900

    第四問

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,6 +3,15 @@ import sys
 
 import pygame as pg
 
+
+# 練習４
+delta = {
+    pg.K_UP: (0, -1),
+    pg.K_DOWN: (0, +1),
+    pg.K_LEFT: (-1, 0),
+    pg.K_RIGHT: (+1, 0),
+    }
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -10,6 +19,9 @@ def main():
     bg_img = pg.image.load("ProjExD2023/ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ProjExD2023/ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rect = kk_img.get_rect()
+    kk_rect.center = 900, 400
+    
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)
     bb_img.set_colorkey((0,0,0))
@@ -24,10 +36,15 @@ def main():
                 return 0
 
         tmr += 1
+        key_lst = pg.key.get_pressed()
+        for k, mv in delta.items():
+            if key_lst[k]:
+                kk_rect.move_ip(mv)
+            
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        bb_rect.move_ip(vx, vy)
-        screen.blit(bb_img, bb_rect)
+        screen.blit(kk_img, kk_rect) # 練習４
+        bb_rect.move_ip(vx, vy) 
+        screen.blit(bb_img, bb_rect) 
         pg.display.update()
         clock.tick(1000)
 
