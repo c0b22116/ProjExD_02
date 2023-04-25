6073035aa4d3df3b075d83c477b74af8162d5db2
Nishimura Takuma <c0b2211617@edu.teu.ac.jp>
2023-04-25 15:45:17 +0900

    第六問

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -69,6 +69,8 @@ def main():
         if not tate:
             vy *= -1
         screen.blit(bb_img, bb_rect) 
+        if kk_rect.colliderect(bb_rect):
+            return
         pg.display.update()
         clock.tick(1000)
 
